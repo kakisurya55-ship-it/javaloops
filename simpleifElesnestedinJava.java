76f57e17b1203764a7e6d9f1dd5ab43612d7a3b5
surya kaki <suryakaki71@gmail.com>
2025-10-25 15:01:23 +0530

    data nested is added

---
+++ simpleifElesnestedinJava.java
@@ -0,0 +1,18 @@
+public class simpleifElesnestedinJava {
+    public static void main (String[] args) {
+
+        int data=95;
+        if (data >= 95) {
+            System.out.println("Need more data:" +data);
+
+        }
+        else if (data >= 85) {
+
+            System.out.println("Need More data for:" +data);
+        }
+
+        else {
+            System.out.println("Need More Data:" +data);
+        }
+    }
+}
