a7fdc42ef977e0c3b809155c5cdb0b59cb255bea
surya kaki <suryakaki71@gmail.com>
2025-10-25 15:16:26 +0530

    data cases

---
+++ Datacases.java
@@ -0,0 +1,19 @@
+public class Datacases {
+    public static void main (String[] args) {
+
+        int data =5;
+        switch (data) {
+            case 1: System.out.println("Data is valid:" +data); break;
+
+            case 2: System.out.println("Data is valid:"  +data); break;
+            case 3: System.out.println("Data is valid:" +data); break;
+
+            case 4: System.out.println("Data is yes valid:" +data); break;
+
+
+
+            case 5: System.out.println("Data id valis:" +data);
+
+        }
+    }
+}
