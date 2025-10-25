17c784cea1e5c3c25a784559168abb1086375843
surya kaki <suryakaki71@gmail.com>
2025-10-25 14:47:30 +0530

    Simple If in condiations Statemdents in java

---
+++ simpleIF.java
@@ -0,0 +1,14 @@
+public class simpleIF {
+    public static void main (String[] args) {
+
+        int a=5;
+        if (a>4) {
+            System.out.println("Values is +ve:" +a);
+
+        }
+        else {
+
+            System.out.println("Values is :" +a);
+        }
+    }
+}
